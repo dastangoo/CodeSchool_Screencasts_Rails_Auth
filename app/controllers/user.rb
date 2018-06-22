32be9a91e424e4c86844b9a5cef1e5887e8cbd9d
dastangoo <hamed.dastangoo@gmail.com>
2018-06-22 10:51:53 +0430

    Rails Auth - User States

---
+++ app/controllers/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord
+  include AASM
+  has_secure_password
+  
+  aasm do 
+    state :pending_activation, initial: true
+    state :active
+    state :removed
+    
+    event :activate do
+      transitions from: :pending_activation, to: :active
+    end
+    
+    event :remove do
+      transitions from: [:pending_activation, :active], to: :removed
+    end
+  end
+end
